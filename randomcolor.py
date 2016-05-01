0b5f7fb192d6a060e52e13eceffc1295cbedd17b
Kevin Wu <me@kevinformatics.com>
2016-04-30 17:23:40 -0700

    more pythonic variable naming

--- randomcolor.py
+++ randomcolor.py
@@ -19,14 +19,14 @@ class RandomColor(object):
 
         for color_name, color_attrs in self.colormap.items():
             lower_bounds = color_attrs['lower_bounds']
-            sMin = lower_bounds[0][0]
-            sMax = lower_bounds[len(lower_bounds) - 1][0]
+            s_min = lower_bounds[0][0]
+            s_max = lower_bounds[len(lower_bounds) - 1][0]
 
-            bMin = lower_bounds[len(lower_bounds) - 1][1]
-            bMax = lower_bounds[0][1]
+            b_min = lower_bounds[len(lower_bounds) - 1][1]
+            b_max = lower_bounds[0][1]
 
-            self.colormap[color_name]['saturation_range'] = [sMin, sMax]
-            self.colormap[color_name]['brightness_range'] = [bMin, bMax]
+            self.colormap[color_name]['saturation_range'] = [s_min, s_max]
+            self.colormap[color_name]['brightness_range'] = [b_min, b_max]
 
     def generate(self, hue=None, luminosity=None, count=1, format_='hex'):
         colors = []
@@ -66,31 +66,31 @@ class RandomColor(object):
 
         saturation_range = self.get_saturation_range(hue)
 
-        sMin = saturation_range[0]
-        sMax = saturation_range[1]
+        s_min = saturation_range[0]
+        s_max = saturation_range[1]
 
         if luminosity == 'bright':
-            sMin = 55
+            s_min = 55
         elif luminosity == 'dark':
-            sMin = sMax - 10
+            s_min = s_max - 10
         elif luminosity == 'light':
-            sMax = 55
+            s_max = 55
 
-        return self.random_within([sMin, sMax])
+        return self.random_within([s_min, s_max])
 
     def pick_brightness(self, H, S, luminosity):
-        bMin = self.get_minimum_brightness(H, S)
-        bMax = 100
+        b_min = self.get_minimum_brightness(H, S)
+        b_max = 100
 
         if luminosity == 'dark':
-            bMax = bMin + 20
+            b_max = b_min + 20
         elif luminosity == 'light':
-            bMin = (bMax + bMin) / 2
+            b_min = (b_max + b_min) / 2
         elif luminosity == 'random':
-            bMin = 0
-            bMax = 100
+            b_min = 0
+            b_max = 100
 
-        return self.random_within([bMin, bMax])
+        return self.random_within([b_min, b_max])
 
     def set_format(self, hsv, format_):
         if 'hsv' in format_:
