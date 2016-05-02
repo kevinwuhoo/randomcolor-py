3f46bddf03a5ed23e6dfa5fe20e3c0437d999eab
Kevin Wu <me@kevinformatics.com>
2016-05-02 00:30:08 -0700

    Bump version, remove find_packages import

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from setuptools import setup, find_packages
+from setuptools import setup
 import os
 
 setup(
     name='randomcolor',
-    version='0.4.4.0',
+    version='0.4.4.1',
     description='For generating attractive random colors',
     long_description=open(os.path.join(os.path.dirname(__file__), 'README.md')).read(),
     keywords='python random color generator',
